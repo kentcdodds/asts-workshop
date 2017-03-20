6aa33fcf33a47030223656541b6782f37f6a577d
Kent C. Dodds <kent@doddsfamily.us>
2017-03-20 16:06:00 -0600

    eslint filler

---
+++ exercises-final/04_eslint.js
@@ -0,0 +1,12 @@
+module.exports = {
+  meta: {
+    docs: {
+      description: '',
+      category: '',
+      recommended: true || false, // up to you :)
+    },
+    fixable: '',
+    schema: [], // extra credit, make this take options
+  },
+  create() {},
+}
